726689897005cba19e15dfa1fc7c332a0e04e873
k2511 <bhongale.k2511@gmail.com>
2025-08-20 20:44:47 +0530

    final chnages

--- src/components/ScrollToHash.js
+++ src/components/ScrollToHash.js
@@ -1,18 +1,50 @@
-// ScrollToHash.js
+// // ScrollToHash.js
+// import { useEffect } from "react";
+// import { useLocation } from "react-router-dom";
+
+// export default function ScrollToHash() {
+//   const { hash } = useLocation();
+
+//   useEffect(() => {
+//     if (hash) {
+//       const element = document.querySelector(hash);
+//       if (element) {
+//         element.scrollIntoView({ behavior: "smooth" });
+//       }
+//     }
+//   }, [hash]);
+
+//   return null;
+// }
+
+
+// import { useEffect } from "react";
+// import { useLocation } from "react-router-dom";
+
+// export default function ScrollToTop() {
+//   const { pathname } = useLocation();
+
+//   useEffect(() => {
+//     window.scrollTo(0, 0); // scrolls to top of the page
+//   }, [pathname]);
+
+//   return null;
+// }
+
+
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-export default function ScrollToHash() {
-  const { hash } = useLocation();
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
 
   useEffect(() => {
-    if (hash) {
-      const element = document.querySelector(hash);
-      if (element) {
-        element.scrollIntoView({ behavior: "smooth" });
-      }
-    }
-  }, [hash]);
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth" // makes the scroll smooth
+    });
+  }, [pathname]);
 
   return null;
 }
